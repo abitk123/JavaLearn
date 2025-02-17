3930dacd5f6f3a96dc301949a893c06d941613d9
Andrei Bitkov <102732533+abitk123@users.noreply.github.com>
2025-02-17 20:17:23 +0100

    change tests

--- src/main/java/DeliveryCalculator.java
+++ src/main/java/DeliveryCalculator.java
@@ -1,7 +1,7 @@
 public class DeliveryCalculator {
 
     public static double calculateDeliveryCost(double distance, boolean isLargeSize, boolean isFragile, String workload) {
-        if (distance < 0) {
+        if (distance <= 0) {
             throw new IllegalArgumentException("Distance cannot be negative");
         }
         if (workload == null || workload.trim().isEmpty()) {
@@ -15,6 +15,7 @@ public class DeliveryCalculator {
         if (isFragile && distance > 30) {
             throw new IllegalArgumentException("Fragile goods cannot be delivered over a distance of more than 30 km");
         }
+
 
         double cost = 0;
 
--- src/test/java/DeliveryCalculatorTests.java
+++ src/test/java/DeliveryCalculatorTests.java
@@ -14,7 +14,9 @@ class DeliveryCalculatorTest {
     @DisplayName("Exception throw for fragile cargo at a distance of more than 30 km")
     @Description("Checks that if the cargo is fragile and the distance is more than 30 km, an exception is thrown.")
     @CsvSource({
-            "35, false, true, ordinary"
+            "9999, false, true, ordinary",
+            "30.01, true, true, ordinary",
+
     })
     void testFragileOver30Km(double distance, boolean isLarge, boolean isFragile, String workload) {
         Exception exception = assertThrows(
@@ -43,27 +45,17 @@ class DeliveryCalculatorTest {
     @DisplayName("Check the cost")
     @Description("Checks the cost calculation for small fragile cargo with different loading conditions.")
     @CsvSource({
-            "5, false, true, very high, 800",
-            "30, false, true, increased, 720",
-            "1, true, true, ordinary, 550",
-            "1, false, true, ordinary, 450",
+            "10, false, true, very high, 800",
+            "11, false, true, increased, 720",
+            "3, true, true, increased, 720",
+            "2, true, true, ordinary, 550",
+            "0.1, false, true, ordinary, 450",
             "31, false, false, very high, 640",
             "31, true, false, high, 700",
+            "9999, false, false, high, 560"
 
     })
     void testSmallSizeWithDifferentWorkloads(double distance, boolean isLarge, boolean isFragile, String workload, double expected) {
-        double cost = DeliveryCalculator.calculateDeliveryCost(distance, isLarge, isFragile, workload);
-        assertEquals(expected, cost);
-    }
-
-    @ParameterizedTest
-    @Tag("calculation")
-    @DisplayName("Cost calculation for long distances without fragility")
-    @Description("Checks delivery costs for distances over 30 km without fragility and with different levels of congestion.")
-    @CsvSource({
-            "31, false, false, high, 560"
-    })
-    void testVeryLongDistance(double distance, boolean isLarge, boolean isFragile, String workload, double expected) {
         double cost = DeliveryCalculator.calculateDeliveryCost(distance, isLarge, isFragile, workload);
         assertEquals(expected, cost);
     }
@@ -73,7 +65,8 @@ class DeliveryCalculatorTest {
     @DisplayName("Errors handling")
     @Description("Proves that the method correctly handles uncorrected input data.")
     @CsvSource({
-            "-1, false, false, ordinary",
+            "-1, false, false, 'ordinary'",
+            "0, false, false, 'ordinary'",
             "10, false, false, ''",
             "10, false, false, 'invalid'"
     })
